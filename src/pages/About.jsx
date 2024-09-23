e4e7353f4dca95c0bf02fb2dc9c8fc40cf4b75ae
Axel Loeuillet <aloeuillet93@gmail.com>
2024-09-23 22:00:29 +0200

    adding banner about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,12 @@
 
 const About = () => {
     return (
-        <div>About</div>
+        <div className="About">
+            <div className="about-banner">
+                <img src="src/assets/images/kalen-emsley-Bkci_8qcdvQ-unsplash 2.jpg" alt="Magnifique paysage de montagnes et de forêts" />
+            </div>
+        </div>
+
     )
 }
 
